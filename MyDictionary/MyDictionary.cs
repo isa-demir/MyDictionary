23d3e596fba3b4bf587ac2c32564ce1e12df17ef
isademir.personal@outlook.com <isademir.personal@outlook.com>
2021-08-25 00:58:23 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<K, V> //K -> keys, V -> values
+    {
+        K[] Keys;
+        K[] tempKeys; //temp diziler, asıl dizileri yeniden boyutlandırdığımızda verileri saklıyor.
+
+        V[] Values;
+        V[] tempValues;
+
+        private bool kontrol;
+
+        public int Count { get { return Keys.Length; } } //Anahtar ve değer sayıları aynı olduğu için bu yeterli.
+
+        public MyDictionary()
+        {
+            Keys = new K[0];
+            Values = new V[0];
+        }
+
+        public void Add(K key, V value) //Ekleme metodu
+        {
+            tempKeys = Keys;
+            tempValues = Values;
+
+            foreach (var item in Keys)
+            {
+                if (item.Equals(key)) //Anahtarlar aynı olamaz. Bu yüzden kontrolünü yaptım.
+                {
+                    kontrol = true;
+                    break;
+                }
+                else
+                {
+                    kontrol = false;
+                }
+            }
+
+            if (kontrol == false)
+            {
+                Keys = new K[Keys.Length + 1];
+                Values = new V[Keys.Length + 1];
+
+                for (int i = 0; i < tempKeys.Length; i++)
+                {
+                    Keys[i] = tempKeys[i];
+                    Values[i] = tempValues[i];
+                }
+
+                Keys[Keys.Length - 1] = key;
+                Values[Keys.Length - 1] = value;
+            }
+
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dictionary<int, string> deneme = new Dictionary<int, string>();
+            deneme.Add(13, "ahmet");
+
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "İsa");
+            myDictionary.Add(2, "Ahmet");
+            myDictionary.Add(3, "Yaren");
+            myDictionary.Add(4, "Sena");
+
+
+            Console.WriteLine(myDictionary.Count);
+
+
+        }
+    }
+ }
+
+
